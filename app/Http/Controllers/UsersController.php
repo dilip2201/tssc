3117027f7de3fef8314c3d51752170218b0aca45
Dhruv Joshi <dhruv.joshi@whizsolutions.co.uk>
2021-06-27 01:39:58 +0530

    user crud and status changes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Validator;
 class UsersController extends Controller
 {
      /**
@@ -24,5 +27,222 @@ class UsersController extends Controller
     public function index()
     {
         return view('users.index');
+    }
+
+    public function getAll(Request $request)
+    {
+        $data = [];
+        $params = $request;
+        $users = new User;
+        if (!empty($params['search']['value'])) {
+            $value = "%" . $params['search']['value'] . "%";
+            $users = $users->where('name', 'like', (string)$value);
+            $users = $users->orWhere('email', 'like', (string)$value);
+        }
+        $users = $users->offset($params['start'])->take($params['length']);
+        $users = $users->get()->except(Auth::id());
+        $userCount = $users->count();
+        $totalRecords = $userCount;
+        foreach ($users as $row) {
+            $id = encrypt($row->id);
+            $status = '<a class="label font-weight-bold label-lg  label-light-danger label-inline changestatus" style="cursor : pointer;" title="cilck to active" data-status="active" data-id="'.$id.'">Inactive</a>';
+            if($row->status == 'active') {
+               $status = '<span class="label font-weight-bold label-lg  label-light-success label-inline changestatus" style="cursor : pointer;" title="cilck to inactive" data-status="inactive" data-id="'.$id.'">Active</span>';
+            }
+            $role = '<span class="label label-success label-dot mr-2"></span><span class="font-weight-bold text-success">Superadmin</span>';
+            if($row->role == 'dashboard') {
+                $role = '<span class="label label-primary label-dot mr-2"></span><span class="font-weight-bold text-primary">Dashboard</span>';
+            }
+            if($row->role == 'data_access_only') {
+                $role = '<span class="label label-warning label-dot mr-2"></span><span class="font-weight-bold text-warning">Data Access Only</span>';
+            }
+            if($row->role == 'data_access_without_import_export') {
+                $role = '<span class="label label-danger label-dot mr-2"></span><span class="font-weight-bold text-danger">Data Access without Import/Export</span>';
+            }
+            $actions = '<span style="overflow: visible; position: relative; width: 125px;">
+                            <a href="javascript:void(0);" class="btn btn-sm btn-clean btn-icon mr-2 btn-add-user" data-target=".add_modal" data-toggle="modal" data-id="'.$id.'" title="Edit details">
+                            <span class="svg-icon svg-icon-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <rect x="0" y="0" width="24" height="24"></rect><path d="M8,17.9148182 L8,5.96685884 C8,5.56391781 8.16211443,5.17792052 8.44982609,4.89581508 L10.965708,2.42895648 C11.5426798,1.86322723 12.4640974,1.85620921 13.0496196,2.41308426 L15.5337377,4.77566479 C15.8314604,5.0588212 16,5.45170806 16,5.86258077 L16,17.9148182 C16,18.7432453 15.3284271,19.4148182 14.5,19.4148182 L9.5,19.4148182 C8.67157288,19.4148182 8,18.7432453 8,17.9148182 Z" fill="#000000" fill-rule="nonzero" transform="translate(12.000000, 10.707409) rotate(-135.000000) translate(-12.000000, -10.707409) "></path>
+                            <rect fill="#000000" opacity="0.3" x="5" y="20" width="15" height="2" rx="1"></rect>
+                            </g></svg></span>
+                            </a>
+                            <a href="javascript:void(0);" class="btn btn-sm btn-clean btn-icon delete-user" data-id="'.$id.'" title="Delete">
+                            <span class="svg-icon svg-icon-md">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <rect x="0" y="0" width="24" height="24"></rect>
+                            <path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" fill="#000000" fill-rule="nonzero"></path>
+                            <path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3"></path></g></svg></span>
+                            </a>
+                       </span>';
+            $rowData['userId'] = $row->userId;
+            $rowData['name'] = $row->name;
+            $rowData['email'] = $row->email;
+            $rowData['role'] = $role;
+            $rowData['permission_period'] = (!empty($row->permission_period)) ? date('d M Y',strtotime($row->permission_period)) : 'N/A';
+            $rowData['status'] = $status;
+            $rowData['action'] = $actions;
+            $data[] = $rowData;
+        }
+        $json_data = array(
+            "draw" => $params['draw'],
+            "recordsTotal" => $totalRecords,
+            "recordsFiltered" => $totalRecords,
+            "data" => $data   // total data array
+        );
+        return json_encode($json_data);
+    }
+
+    /**
+     * Get model for add edit user
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function getModal(Request $request)
+    {
+        $user = array();
+        if (isset($request->id) && $request->id != '') {
+            $id = decrypt($request->id);
+            $user = User::where('id',$id)->first();
+
+        }
+        return view('users.getmodal', compact('user'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email',
+            'role' => 'required',
+            'permission_period' => 'required',
+        ];
+        if (isset($request->userid)) {
+            $userid = decrypt($request->userid);
+            $rules['email'] = 'required|unique:users,email,'.$userid;
+        }else{
+            $rules['email'] = 'required|unique:users,email';
+            $rules['password'] = 'min:6|required_with:password_confirmation|same:password_confirmation';
+        }
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $arr = array("status" => 400, "msg" => $validator->errors()->first(), "result" => array());
+        } else {
+            try {
+                if (isset($userid)) {
+                    $user = User::find($userid);
+                    $msg = "User updated successfully.";
+                }else{
+                    $user = new User;
+                    $maxuserid = User::max('userId');
+                    $newuserid = $maxuserid + 1;
+                    $user->userId = '00'.$newuserid;
+                    $msg = "User added successfully.";
+                }
+                $user->name = $request->name;
+                $user->email = $request->email;
+                $user->role = $request->role;
+                $user->status = $request->status ?? 'inactive';
+                $user->permission_period = $request->permission_period;
+                if(isset($request->password) && !empty($request->password)){
+                    $user->password = Hash::make($request->password);
+                }
+                $user->save();
+                $arr = array("status" => 200, "msg" => $msg);
+            } catch (\Illuminate\Database\QueryException $ex) {
+                $msg = $ex->getMessage();
+                if (isset($ex->errorInfo[2])) :
+                    $msg = $ex->errorInfo[2];
+                endif;
+                $arr = array("status" => 400, "msg" => $msg, "result" => array());
+            } catch (Exception $ex) {
+                $msg = $ex->getMessage();
+                if (isset($ex->errorInfo[2])) :
+                    $msg = $ex->errorInfo[2];
+                endif;
+                $arr = array("status" => 400, "msg" => $msg, "result" => array());
+            }
+        }
+
+        return \Response::json($arr);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $id = decrypt($id);
+            $user = User::find($id);
+            if ($user) {
+                $user->delete();
+                $arr = array("status" => 200, "msg" => 'User deleted successfully.');
+            } else {
+                $arr = array("status" => 400, "msg" => 'User not found. please try again!');
+            }
+
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $msg = 'You can not delete this as related data are there in system.';
+            if (isset($ex->errorInfo[2])) {
+                $msg = $ex->errorInfo[2];
+            }
+            $arr = array("status" => 400, "msg" => $msg, "result" => array());
+        } catch (Exception $ex) {
+            $msg = 'You can not delete this as related data are there in system.';
+            if (isset($ex->errorInfo[2])) {
+                $msg = $ex->errorInfo[2];
+            }
+            $arr = array("status" => 400, "msg" => $msg, "result" => array());
+        }
+        return \Response::json($arr);
+    }
+
+    /**
+     * Change status of user active or inactive
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response send response in json
+     */
+    public function changeStatus(Request $request)
+    {
+        try {
+            $id = decrypt($request->id);
+            $user = User::find($id);
+            if ($user) {
+                $user->update(['status' => $request->status]);
+                $arr = array("status" => 200, "msg" => 'User status changed successfully.');
+            } else {
+                $arr = array("status" => 400, "msg" => 'User not found. please try again!');
+            }
+
+        } catch (\Illuminate\Database\QueryException $ex) {
+
+            $msg = $ex->getMessage();
+            if (isset($ex->errorInfo[2])) {
+                $msg = $ex->errorInfo[2];
+            }
+
+            $arr = array("status" => 400, "msg" => $msg, "result" => array());
+        } catch (Exception $ex) {
+
+            $msg = $ex->getMessage();
+            if (isset($ex->errorInfo[2])) {
+                $msg = $ex->errorInfo[2];
+            }
+            $arr = array("status" => 400, "msg" => $msg, "result" => array());
+        }
+        return \Response::json($arr);
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,11 +1,7 @@
 @extends('layout.app')
 @section('action')
 <div class="d-flex align-items-center">
-	<div class="dropdown dropdown-inline ml-2" data-toggle="tooltip" title="Quick actions" data-placement="top">
-		<a href="#" class="btn btn-white font-weight-bold py-3 px-6" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Add New</a>
-		
-	</div>
-	<!--end::Dropdown-->
+    <a href="javascript:void(0);" class="btn btn-white font-weight-bold py-3 px-6 btn-add-user" data-id="" data-target=".add_modal" data-toggle="modal" >Add New</a>
 </div>
 <!--end::Toolbar-->
 @endsection
@@ -15,144 +11,45 @@
 <!--begin::Container-->
 <div class="container">
 	<div class="card card-custom">
-									
-									<div class="card-body">
-										<!--begin: Datatable-->
-										<table class="table table-separate table-head-custom table-checkable kt_datatable_2" id="">
-											<thead>
-												<tr>
-													<th>Record ID</th>
-													<th>Order ID</th>
-													<th>Country</th>
-													<th>Ship City</th>
-													<th>Ship Address</th>
-													<th>Actions</th>
-												</tr>
-											</thead>
-											<tbody>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-												<tr>
-													<td>1</td>
-													<td>1</td>
-													<td>64616-103</td>
-													<td>Brazil</td>
-													<td>São Félix do Xingu</td>
-													<td>698 Oriole Pass</td>
-													<td nowrap="nowrap"></td>
-												</tr>
-											</tbody>
-										</table>
-										<!--end: Datatable-->
-									</div>
-								</div>
+        <div class="card-body">
+            <!--begin: Datatable-->
+            <table id="users" class="table table-separate tableme-head-custom table-checkable kt_datatable_2" id="">
+                <thead>
+                    <tr>
+                        <th>User ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Permission Period</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+			<!--end: Datatable-->
+		</div>
+	</div>
 <!--end::Container-->
+    <!-- modal -->
+    <div class="modal fade add_modal" >
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title modaltitle"></h5>
+                </div>
+                <div class="modal-body addeditform">
+                    <div class="col-sm-12 col-md-12" style="text-align: center">
+                        <span class="spinner-success spinner-lg addformspinner"></span>
+                    </div>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
 </div>
 
 @push('link')
@@ -160,11 +57,179 @@
 @endpush
 
 @push('script')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/attempt-to-update-packagist/pnotify.js" integrity="sha512-vVCwjYtarAac2AMUNPP0cqRITQ00L8kXCRzUfLInqdz3iPUa/3kuBiXjhcEG4VaBLsBzgcChpq68qzUl1LAZ4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="{{ URL::asset('public/assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
 <script type="text/javascript">
-	$('.kt_datatable_2').DataTable({
 
-	});
+	$(document).ready(function () {
+
+        /* datatable */
+        $("#users").DataTable({
+            "responsive": true,
+            "autoWidth": false,
+            processing: true,
+            serverSide: true,
+            stateSave: true,
+            ajax: {
+                'url': "{{ route('users.getall') }}",
+                'type': 'POST',
+                'data': function (d) {
+                    d._token = "{{ csrf_token() }}";
+                }
+            },
+            columns: [
+                {data: 'userId'},
+                {data: 'name'},
+                {data: 'email'},
+                {data: 'role',"orderable": false},
+                {data: 'permission_period'},
+                {data: 'status',"orderable": false},
+                {data: 'action', "orderable": false},
+            ]
+        });
+        /********* Add New User ************/
+        $('body').on('click', '.btn-add-user', function () {
+            var id = $(this).data('id');
+            if (id == '') {
+                $('.modaltitle').text('Add User');
+            } else {
+                $('.modaltitle').text('Edit User');
+            }
+            $.ajax({
+                url: "{{ route('users.getmodal')}}",
+                type: 'POST',
+                headers: {
+                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                },
+                data: {id: id},
+                beforeSend: function () {
+                    $('.addformspinner').addClass('spinner');
+                },
+                success: function (data) {
+                    $('.addeditform').html(data);
+                    $(".formsubmit").validate({
+                        rules: {
+                            "#password": {
+                                required: true,
+                                minlength: 6
+                            },
+                            "#password_confirmation": {
+                                required: true,
+                                minlength: 6,
+                                equalTo: "#password"
+                            }
+                        }
+                    });
+                },
+            });
+        });
+        /*********** Cancel form ***********/
+        $('body').on('click', '.resetaddeditform', function () {
+            let html = '<div class="col-sm-12 col-md-12" style="text-align: center">'+
+                            '<span class="spinner-success spinner-lg addformspinner"></span>'+
+                        '</div>';
+            $('.addeditform').html(html);
+        });
+        /********** Submit form ************/
+        $('body').on('submit', '.formsubmit', function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: $(this).attr('action'),
+                data: new FormData(this),
+                type: 'POST',
+                contentType: false,
+                cache: false,
+                processData: false,
+                beforeSend: function () {
+                    $('.submitbutton').addClass('spinner');
+                },
+                success: function (data) {
+
+                    if (data.status == 400) {
+                        $('.submitbutton').removeClass('spinner');
+                        toastr.error(data.msg)
+                    }
+                    if (data.status == 200) {
+                        $('.submitbutton').removeClass('spinner');
+                        $('.add_modal').modal('hide');
+                        $('#users').DataTable().ajax.reload();
+                        toastr.success(data.msg,'Success!')
+                    }
+                },
+            });
+        });
+        /********** Delete User ************/
+        $('body').on('click', '.delete-user', function () {
+            var id = $(this).data('id');
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You want to delete the user.",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonText: "Yes"
+            }).then(function(result) {
+                if (result.value) {
+                    $.ajax({
+                        url: '{{ url("users/delete") }}/' + id,
+                        type: 'GET',
+                        headers: {
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                        },
+                        success: function (data) {
+                            if (data.status == 400) {
+                                Swal.fire("Oh no!", "Something went wrong!", "error");
+                            }
+                            if (data.status == 200) {
+                                $("#users").DataTable().ajax.reload();
+                                Swal.fire(
+                                    "Deleted!",
+                                    data.msg,
+                                    "success"
+                                )
+                            }
+                        },
+                        error: function () {
+                            Swal.fire("Oh no!", "Something went wrong!", "error");
+                        }
+                    });
+                }
+            });
+        });
+        /********** change status **********/
+        $('body').on('click', '.changestatus', function () {
+            var id = $(this).data('id');
+            var status = $(this).data('status');
+            Swal.fire({
+                    title: "Are you sure?",
+                    text: "You want to "+status+" the user.",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Yes"
+            }).then(function(result) {
+                if (result.value) {
+                    $.ajax({
+                        url: '{{ route("users.changestatus") }}',
+                        type: 'POST',
+                        headers: {
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                        },
+                        data: {id: id, status: status},
+                        success: function (data) {
+                            $("#users").DataTable().ajax.reload();
+                            Swal.fire(
+                                "Changed!",
+                                "Status has been changed.",
+                                "success"
+                            )
+                        },
+                        error: function () {
+                            Swal.fire("Oh no!", "Something went wrong!", "error");
+                        }
+                    });
+                }
+            });
+        });
+    });
 </script>
 
 @endpush
